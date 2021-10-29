359fba04773b43af0d5867c4d878dd6bde24f643
qilei <lei.qi@duobei.com>
2021-10-29 17:10:03 +0800

    feat: 页面加载

--- docs/example/README.md
+++ docs/example/README.md
@@ -3,7 +3,8 @@
 计划是很多的 但是拆分出来的很少...
 
 
-[贝塞尔曲线平滑canvas划线](https://heretic-g.github.io/demo/canvas-draw-line.html)
+[贝塞尔曲线平滑canvas划线](https://heretic-g.github.io/example/canvas-draw-line.html)
 
-[监听devtool是否打开](https://heretic-g.github.io/demo/demo/devtool.html)
+[监听devtool是否打开](https://heretic-g.github.io/example/devtool.html)
 
+[页面加载进度](https://heretic-g.github.io/example/pageLoading/index.html)
---
+++ docs/example/pageLoading/serviceWorker.js
@@ -0,0 +1,41 @@
+
+const broadcast = new BroadcastChannel('count-channel');
+self.addEventListener('install', function(event) {
+  self.skipWaiting()
+})
+self.addEventListener('activate', function(event) {
+  self.clients.claim()
+})
+
+
+broadcast.addEventListener('message', function (payload) {
+  const now = performance.now()
+  switch (payload.data.type) {
+    case 'init':
+      console.log('init 初始化开始')
+      for (let key in broadcastReq) {
+        if (broadcastReq[key] < now - payload.data.payload) {
+          delete broadcast[key]
+        }
+      }
+      broadcast.postMessage({ type: 'ack', payload: Object.keys(broadcastReq)})
+      break
+    case 'ack':
+      console.log(`已经确认加载资源`, payload.data.payload)
+      payload.data.payload.forEach(curr => {
+        delete broadcastReq[curr]
+      })
+      break
+  }
+})
+
+let broadcastReq = {}
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.method.toUpperCase() !== 'HEAD') {
+    if (event.request.url.indexOf('chrome-extension') !== 0) {
+      broadcastReq[event.request.url] = self.performance.now()
+      broadcast.postMessage({ type: 'fetch', payload: Object.keys(broadcastReq) })
+    }
+  }
+});
